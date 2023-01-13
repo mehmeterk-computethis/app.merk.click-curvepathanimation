0d18846d35639ccbf6d45a1aba4cf82c4cb7c2e7
mehmeterk-computethis <34861665+mehmeterk-computethis@users.noreply.github.com>
2023-01-13 09:48:05 +0000

    Refined

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import Nav from './Nav';
-import Footer from './Footer';
-import DeviceOri from './DeviceOri'
+import'./App.css';
+import PathGenerator from './PathGenerator'
+
 import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Nav/>
-
-    <Footer/>
+      <PathGenerator/>
   </React.StrictMode>
 );
 
